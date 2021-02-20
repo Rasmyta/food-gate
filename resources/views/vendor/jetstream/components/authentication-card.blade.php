fdeca18c9375bd96385c0837daadf93024fa961e
Rasma <rasmabu@gmail.com>
2021-02-20 23:55:55 +0100

    Change logo

--- resources/views/vendor/jetstream/components/authentication-card.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card.blade.php
@@ -1,6 +1,6 @@
 <div class="container">
     <div class="row justify-content-center my-5">
-        <div class="col-sm-12 col-md-8 col-lg-5 my-5">
+        <div class="col-sm-12 col-md-8 col-lg-5">
             <div>
                 {{ $logo }}
             </div>
